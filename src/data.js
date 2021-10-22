5ee8cf544dc398c62743e2f33b932026a08a20e3
nishchal singh <singhnishchal27@gmail.com>
2021-10-22 19:18:30 +0530

    Update data.js

--- src/data.js
+++ src/data.js
@@ -3,32 +3,32 @@ export const featuredPortfolio = [
 {
     id: 1,
     title: 'Art & Design',
-    img: "assets/port-img1.png",
+    img: "assets/images/port-img1/port-img1.png",
 },
 {
     id: 2,
     title: 'Technology',
-    img: "assets/port-img2.png",
+    img: "assets/images/port-img2/port-img2.png",
 },
 {
     id: 3,
     title: 'Food & Restaurant',
-    img: "assets/port-img3.png",
+    img: "assets/images/port-img3/port-img3.png",
 },
 {
     id: 4,
     title: 'Business',
-    img: "assets/port-img4.png",
+    img: "assets/images/port-img4/port-img4.png",
 },
 {
     id: 5,
     title: 'Travel & Hotel',
-    img: "assets/port-img5.png",
+    img: "assets/images/port-img5/port-img5.png",
 },
 {
     id: 6,
     title: 'Portfolio',
-    img: "assets/port-img6.png",
+    img: "assets/images/port-img6/port-img6.png",
 },
 
 ];
@@ -37,32 +37,32 @@ export const webPortfolio = [
 {
     id: 1,
     title: 'Full Width Slider',
-    img: "assets/design-img1.png",
+    img: "assets/images/design-img1/design-img1.png",
 },
 {
     id: 2,
     title: 'Text On Image',
-    img: "assets/design-img2.png",
+    img: "assets/images/design-img2/design-img2.png",
 },
 {
     id: 3,
     title: 'Wide',
-    img: "assets/design-img3.png",
+    img: "assets/images/design-img3/design-img3.png",
 },
 {
     id: 4,
     title: 'Shapes',
-    img: "assets/design-img4.png",
+    img: "assets/images/design-img4/design-img4.png",
 },
 {
     id: 5,
     title: 'Grid',
-    img: "assets/design-img5.png",
+    img: "assets/images/design-img5/design-img5.png",
 },
 {
     id: 6,
     title: 'Layered Images',
-    img: "assets/design-img6.png",
+    img: "assets/images/design-img6/design-img6.png",
 },
 
 ];
@@ -71,32 +71,32 @@ export const mobilePortfolio = [
 {
     id: 1,
     title: 'React',
-    img: "assets/app-img1.png",
+    img: "assets/images/app-img1/app-img1.png",
 },
 {
     id: 2,
     title: 'Dynamic Website',
-    img: "assets/app-img2.png",
+    img: "assets/images/app-img2/app-img2.png",
 },
 {
     id: 3,
     title: 'Node.js App',
-    img: "assets/app-img3.png",
+    img: "assets/images/app-img3/app-img3.png",
 },
 {
     id: 4,
     title: 'Express.js App',
-    img: "assets/app-img4.png",
+    img: "assets/images/app-img4/app-img4.png",
 },
 {
     id: 5,
     title: 'MongoDB',
-    img: "assets/app-img5.png",
+    img: "assets/images/app-img5/app-img5.png",
 },
 {
     id: 6,
     title: 'Full Stack Developemnt',
-    img: "assets/app-img6.png",
+    img: "assets/images/app-img6/app-img6.png",
 },
 
 ];
